1da2d6eff1bc4683b295522467216ef1eb44b311
alejorod62 <85136358+alejorod62@users.noreply.github.com>
2021-06-07 19:54:35 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-let arreglo_objs_literal = require('./servicios/tareas.json');
-let arreglo_objs_literal2 = JSON.parse(arreglo_objs_literal);
-
-
-console.log (arreglo_objs_literal2);
-
-//agrego esto noma
