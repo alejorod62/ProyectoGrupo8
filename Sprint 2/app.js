3f89637c8c267f8d59e8e9096907e574ac1e9f52
lourdesbo3 <luulibo@gmail.com>
2021-06-29 18:47:40 -0300

    linkeando app.js con el resto de las vistas

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -4,14 +4,19 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
+   res.sendFile(path.join(__dirname, './views/index.html'));
 });
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));
+ });
+
+ app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html'));
+ });
 
 app.use(express.static(path.join(__dirname, './public')));  
 
 app.listen(3002, () => {
     console.log("Servidor corriendo");
 });
-
-
-
