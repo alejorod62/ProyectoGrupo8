8053ef46c87285d89897c929bc4eda7049cbe4fe
alejorod62 <alejorodri62@gmail.com>
2021-07-05 16:00:34 -0300

    responsive y detalles

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/index'));
+   res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
 app.get('/login', (req,res) =>{
-    res.sendFile(path.join(__dirname, './views/login'));
+    res.sendFile(path.join(__dirname, './views/login.html'));
  });
 
  app.get('/register', (req,res) =>{
-    res.sendFile(path.join(__dirname, './views/register'));
+    res.sendFile(path.join(__dirname, './views/register.html'));
  });
 
  app.get('/cart', (req,res) =>{
