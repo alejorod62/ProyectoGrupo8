8e47805d1506db1e22125fa848d4bc4025bd7b2b
lourdesbo3 <luulibo@gmail.com>
2021-07-17 21:25:10 -0300

    cambiando extensiones .ejs, separando header y footer e importandolos

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const mainRouter = require('./src/router/mainRouter')
 
-app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/index.html'));
-});
+app.get('/', mainRouter);
 
 app.get('/login', (req,res) =>{
     res.sendFile(path.join(__dirname, './views/login.html'));
@@ -24,6 +22,8 @@ app.get('/product', (req,res) =>{
 });
 
 app.use(express.static(path.join(__dirname, './public')));  
+
+app.set('view engine', 'ejs')
 
 app.listen(3002, () => {
     console.log("Servidor corriendo");
