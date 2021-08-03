1f9a06051db42eff68340e8a92c94a556d5cfbbd
lourdesbo3 <luulibo@gmail.com>
2021-08-02 23:23:45 -0300

    CRUD y multer de cursos

--- Sprint 2/src/router/cursosRouter.js
+++ Sprint 2/src/router/cursosRouter.js
@@ -1,11 +1,26 @@
 const cursosController = require('.././controller/cursosController');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const configuracionImagen = multer.diskStorage({
+    destination: function(req, file, cb) {
+     cb(null, path.join(__dirname,'../../public/img/cursos')); 
+    },
+    filename: function(req, file, cb) {   
+     let imageName = Date.now() + file.originalname;  
+     cb(null, imageName);         
+    }
+});
+
+const uploadFile = multer({ storage: configuracionImagen});
 
 router.get('/', cursosController.index)
 router.get('/cart', cursosController.carrito)
 router.get('/:id/details', cursosController.detalle)
-router.get('/new', cursosController.nuevo)
+router.get('/new', cursosController.crear)
+router.post('/new', cursosController.guardar); 
 router.get('/:id/edit', cursosController.editar)
 
 module.exports = router;
