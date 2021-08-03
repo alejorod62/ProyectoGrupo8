9c7a01e2fd8b4f8f909e276d75f275fea9ec11be
lourdesbo3 <luulibo@gmail.com>
2021-08-03 00:10:33 -0300

    CRUD productos completo

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -1,19 +1,20 @@
-const mainRouter = require('./src/router/mainRouter')
-const cursosRouter = require('./src/router/cursosRouter')
-const usuariosRouter = require('./src/router/usuariosRouter')
-
+const methodOverride =  require('method-override');
 const express = require('express');
 const path = require('path');
 const app = express();
 
+const mainRouter = require('./src/router/mainRouter')
+const cursosRouter = require('./src/router/cursosRouter')
+const usuariosRouter = require('./src/router/usuariosRouter')
+
 app.use('/', mainRouter);
-
 app.use('/courses', cursosRouter);
-
 app.use('/user', usuariosRouter);
 
 app.use(express.static(path.join(__dirname, './public')));  
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs')
 
 app.listen(process.env.PORT || 3002, () => {
