838b8c2d5fb4cbb762cb38b882e727ebfd386877
Migliore <dmigliore@metrovias.com.ar>
2021-08-09 20:50:25 -0300

    s

--- Sprint 2/src/controller/usuariosController.js
+++ Sprint 2/src/controller/usuariosController.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const profileFilePath = path.join(__dirname, '../database/dataUsuarios.json');
-const profile = JSON.parse(fs.readFileSync(profileFilePath, 'utf-8'));
+const usuariosTotal = JSON.parse(fs.readFileSync(profileFilePath, 'utf-8'));
+
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -17,7 +18,55 @@ const usuariosController = {
     }, 
     registro: (req, res) => {
         res.render('user/register')
-    }    
+    },
+    crear: (req, res) => {
+        res.render('products/new')
+    },
+    editar: (req, res) => {
+        let id = req.params.id;
+        let usuarioElegido
+
+        for (let usuario of usuariosTotal)
+            if (id == usuario.id) {
+                usuarioElegido = usuario 
+            }
+        res.render('user/edit', {usuarioEditable: usuarioElegido})    
+    },
+    modificar: (req, res) => {
+		let id = req.params.id;
+		for (let usuario of usuariosTotal){
+			if (id==usuario.id){ 
+				usuario.nombre= req.body.nombre; 
+				usuario.apellido= req.body.apellido;
+				usuario.email= req.body.email;
+				usuario.contraseña= req.body.contraseña;
+				usuario.telefono= req.body.telefono;
+		}
+		fs.writeFileSync(profileFilePath, JSON.stringify(usuario, null, ' '))
+		res.redirect('/')
+    	}
+    },
+    
+    guardar: (req, res) => {
+		let idNuevo
+		for (usuario of usuariosTotal){
+			if (idNuevo<usuario.id){
+				idNuevo = usuario.id
+			}
+		}
+		idNuevo++
+		let cursoNuevo = {
+			id: idNuevo,
+			nombre: req.body.nombre,
+			apellido: req.body.apellido,
+			email: req.body.email,
+			contrseña: req.body.contraseña,
+            telefono: req.body.telefono,		
+		}
+        fs.writeFileSync(profileFilePath, JSON.stringify(cursoNuevo, null, ' '))
+		res.redirect('/')
+    }
+    
 }
 
 module.exports = usuariosController;
--- Sprint 2/src/router/usuariosRouter.js
+++ Sprint 2/src/router/usuariosRouter.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get('/login', usuariosController.login)
 router.get('/profile', usuariosController.perfil)
 router.get('/register', usuariosController.registro)
-
+router.post('/new', usuariosController.crear);
+router.get('/:id/edit', usuariosController.editar)
+router.put('/:id/edit', usuariosController.modificar);
 module.exports = router;
