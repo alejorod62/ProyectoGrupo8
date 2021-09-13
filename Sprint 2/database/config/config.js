3476111a87bb11eabcf4f492bb8b57c24593cbba
lourdesbo3 <luulibo@gmail.com>
2021-09-13 04:02:13 -0300

    configuracion sequelize

---
+++ Sprint 2/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "bolourdes",
+    "password": "4431alwaysdata",
+    "database": "bolourdes_criptotest",
+    "host": "mysql-bolourdes.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
