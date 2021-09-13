219371e8a646b243ca077a76639fe0235978dc49
lourdesbo3 <luulibo@gmail.com>
2021-09-13 01:10:24 -0300

    seteo de sequelize

--- Sprint 2/src/router/usuariosRouter.js
+++ Sprint 2/src/router/usuariosRouter.js
@@ -5,6 +5,7 @@ const path = require('path');
 const multer = require('multer');
 const logueoMw = require('.././middlewares/logueoMw');
 const profileMw = require('.././middlewares/profileMw');
+const navBarMw = require('.././middlewares/navBarMw');
 
 /* Multer config */
 
@@ -23,7 +24,7 @@ const uploadFile = multer({ storage: configImagenPerfil });
 router.get('/login', logueoMw, usuariosController.login);
 router.post('/login', usuariosController.ingreso);
 router.get('/profile', profileMw, usuariosController.perfil);
-router.get('/register', /*logueoMw,*/ usuariosController.registro);
+router.get('/register', logueoMw, usuariosController.registro);
 router.post('/register', uploadFile.single ('ImagenPerfil') ,usuariosController.guardar);
 router.get('/edit/:id', usuariosController.editar);
 router.put('/edit/:id', usuariosController.modificar);
