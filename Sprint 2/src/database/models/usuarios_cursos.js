3461628b1138c812317c300e583dd3ad233b7f7d
Migliore <dmigliore@metrovias.com.ar>
2021-09-20 20:19:52 -0300

    agrego

---
+++ Sprint 2/src/database/models/usuarios_cursos.js
@@ -0,0 +1,22 @@
+function uscursosData(sequelize, Datatypes){
+
+    alias = 'usuarios_Cursos';
+  
+    cols = {
+      id: {type: Datatypes.INTEGER, primaryKey: true, autoIncrement: true},
+      UsuarioFK: {type: Datatypes.INTEGER(50)},
+      CursosFK: {type: Datatypes.INTEGER},
+      Fecha: {type: Datatypes.DATE},
+      Precio: {type: Datatypes.TINYINT},
+      Medio_PagoFK: {type: Datatypes.INTEGER},
+    }
+  
+    config = {camelCase: false, timestamps: false}; 
+  
+    const usarioscursos = sequelize.define(alias,cols,config)
+  
+    return usuarioscursos;
+    }
+  
+  
+  module.exports = uscursosData;
