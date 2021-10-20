9b3fb34765e3c423e267ae672d1cd051ae12600e
lourdesbo3 <luulibo@gmail.com>
2021-10-20 02:35:42 -0300

    carrito en progreso

--- Sprint 2/public/js/carritologic.js
+++ Sprint 2/public/js/carritologic.js
@@ -1,16 +1,37 @@
-var c = document.getElementsByClassName('producto');
-var b = document.getElementById('agregar-al-carrito');
+window.onload = function () { 
 
-let arrayCursos = [];
-b.onclick = function () {
-    if (arrayCursos == []) {
-        arrayCursos.push(c.precio)
-        sessionStorage.setItem('carrito', arrayCursos)
-        console.log(arrayCursos)
-    } else {
-        arrayCursos = sessionStorage.getItem('carrito')
-        arrayCursos.push(c.precio)
-        sessionStorage.setItem('carrito', arrayCursos)
-        console.log(arrayCursos)
-    }
-}   
+    var c = document.getElementsByClassName('producto');
+    var botCarrito = document.getElementsByName('boton-carrito');
+    var carrito = document.getElementById('lista-carrito');
+    var botComprar = document.getElementById('comprar');
+    var botVaciar = document.getElementById('vaciar');
+
+    botCarrito.addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log(armarCarrito());
+    })
+
+    let arrayCursos = [];
+
+    function armarCarrito () {    
+        if (arrayCursos == []) {
+            arrayCursos.push(c.precio)
+            sessionStorage.setItem('carrito', arrayCursos)
+        } else {
+            arrayCursos = sessionStorage.getItem('carrito')
+            arrayCursos.push(c.precio)
+            sessionStorage.setItem('carrito', arrayCursos)
+        }}
+}
+/*
+    function vaciarCarrito () {
+    botVaciar.onclick = function(e) {
+        event.preventDefault();
+        alert('¿Estas seguro?');
+        if (arrayCursos !== []) {
+            arrayCursos = []
+        } else {
+            res.redirect('/')
+            alert('Tu carrito esta vacío, selecciona algún curso para ver el carrito')
+        }
+    }*/
