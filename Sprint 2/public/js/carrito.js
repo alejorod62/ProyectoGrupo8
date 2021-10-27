f65ae23cda228fb5d3f4a82fa2e47d365fc7fb8b
alejorod62 <alejorodri62@gmail.com>
2021-10-27 15:19:58 -0300

    carrito delete y total andando

--- Sprint 2/public/js/carrito.js
+++ Sprint 2/public/js/carrito.js
@@ -25,6 +25,7 @@ function addItemToShoppingCart(itemTitle, itemPrecio, itemImage, itemCuotas) {
   const shoppingCartContent = `
   <div class="row shoppingCartItem">
         <div class="col-6">
+        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                 <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
             </div>
         </div>
@@ -38,17 +39,21 @@ function addItemToShoppingCart(itemTitle, itemPrecio, itemImage, itemCuotas) {
             <p class="itemCuotas mb-0 shoppingCartItemCuotas">${itemCuotas}</p>
         </div>
     </div>
+    
         <div class="col-4">
-            <div
-                
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
+            <br>
+            <br>
+            
+            <button class="btn btn-danger buttonDelete" type="button">Borrar</button> </div>
     </div>`;
 
     shoppingCartRow.innerHTML = shoppingCartContent;
     shoppingCartItemsContainer.append(shoppingCartRow);
-    
+
+    shoppingCartRow
+    .querySelector('.buttonDelete')
+    .addEventListener('click', removeShoppingCartItem);
+
     updateShoppingCartTotal();
 }
 
@@ -61,16 +66,27 @@ function updateShoppingCartTotal() {
   const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
 
   shoppingCartItems.forEach((shoppingCartItem) => {
-    const shoppingCartItemPrecioElement = shoppingCartItem.querySelector(
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
       '.shoppingCartItemPrecio'
     );
-    const shoppingCartItemPrecio = shoppingCartItemPrecioElement.textContent
-      ;
-
-
-    console.log ('aca : updateShoppingCartTotal -> shoppingCartItemPrecio ' , shoppingCartItemPrecio)
+    console.log(shoppingCartItemPriceElement)
+    const shoppingCartItemPrice = Number(
+      shoppingCartItemPriceElement.textContent.replace('$', '')
+    );
     
-} ) ; }
+    total = total + shoppingCartItemPrice ;
+  });
+  shoppingCartTotal.innerHTML = `$${total.toFixed(2)}`;
+}
+
+function removeShoppingCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.closest('.shoppingCartItem').remove();
+  updateShoppingCartTotal();
+}
+
+    
+
 
 
 
